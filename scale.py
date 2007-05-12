93eebb30fa79782ef1f83e49cc515c6f121f3d6c
marco <marco@nocturne>
2007-05-13 00:50:44 +0100

    Clean up computescaling().

--- scale.py
+++ scale.py
@@ -89,55 +89,49 @@ class Scale:
         toler = 0.01 * nnzs
         rowfactor1, rowfactor2 = zeros(rows), zeros(cols)
         colfactor1, colfactor2 = zeros(cols), zeros(cols)
+        rownnzs, colnnzs = self.rownnzs, self.colnnzs
 
         for i in range(rows):
-            rowfactor1[i] = self.rowlogs[i] / self.rownnzs[i]
+            rowfactor1[i] = self.rowlogs[i] / rownnzs[i]
             rowfactor2[i] = rowfactor1[i]
 
         # initial residual
-        self.rowres = zeros(rows)
-        self.colres = self.collogs.copy()
+        rowres = zeros(rows)
+        colres = self.collogs.copy()
         for i in range(nnzs):
             row, col = A.rowcol(i)
-            self.colres[col] -= rowfactor1[row]
+            colres[col] -= rowfactor1[row]
 
-        sk1, sk = 0.0, self.__updatesk(self.colres, self.colnnzs)
+        sk1, sk = 0.0, self.__updatesk(colres, colnnzs)
         ek1, ek, qk1, qk = 0.0, 0.0, 0.0, 1.0
 
         while sk > toler:
 
-            case = iters % 2
-
-            if case == 0:
-                if iters > 0:
-                    rowfactor2 = rowfactor1.copy()
-                    self.__updatefactors(rowfactor1, rowfactor2, self.rowres,
-                                         self.rownnzs, ek * ek1, qk * qk1)
-                self.__updateresidual(self.rowres, self.colres,
-                                      self.colnnzs, A, ek, qk)
-                sk1, sk = sk, self.__updatesk(self.rowres, self.rownnzs)
-
+            if iters % 2 == 0:
+                factor, oldfac = rowfactor1, rowfactor2
+                res1, res2, nnz1, nnz2 = rowres, colres, rownnzs, colnnzs
             else:
-                colfactor2 = colfactor1.copy()
-                self.__updatefactors(colfactor1, colfactor2, self.colres,
-                                     self.colnnzs, ek * ek1, qk * qk1)
-                self.__updateresidual(self.colres, self.rowres,
-                                      self.rownnzs, A, ek, qk)
-                sk1, sk = sk, self.__updatesk(self.colres, self.colnnzs)
+                factor, oldfac = colfactor1, colfactor2
+                res1, res2, nnz1, nnz2 = colres, rowres, colnnzs, rownnzs
 
+            if iters > 0:
+                self.__updatefactors(factor, oldfac, res1,
+                                     nnz1, ek * ek1, qk * qk1)
+            self.__updateresidual(res1, res2, nnz2, A, ek, qk)
+
+            sk1, sk = sk, self.__updatesk(res1, nnz1)
             ek1, ek = ek, qk * sk / sk1
             qk1, qk = qk, 1.0 - ek
             iters += 1
 
         # final update
         if iters > 0:
-            case = iters % 2
-            if case == 0:
-                self.__updatefactors(rowfactor1, rowfactor2, self.rowres,
-                                     self.rownnzs, ek * ek1, qk1)
+            if iters % 2 == 0:
+                self.__updatefactors(rowfactor1, rowfactor2, rowres,
+                                     rownnzs, ek * ek1, qk1)
             else:
-                self.__updatefactors(colfactor1, colfactor2, self.colres,
-                                     self.colnnzs, ek * ek1, qk1)
+                self.__updatefactors(colfactor1, colfactor2, colres,
+                                     colnnzs, ek * ek1, qk1)
 
         # find the scaling factors
         for row in range(rows):
