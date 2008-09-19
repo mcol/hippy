6287a29e4a0a3b3c5d3863a43970a000854ba1d1
marco <marco@nocturne>
2008-09-19 23:33:18 +0100

    Implement the upper bounds.
    This implements the changes necessary to solve problems with upper bounded
    variables. We add two extra fields to the hippy class to store  the z and w
    iterates, implemented as Sparsevectors. Many interfaces need to be updated
    to accept the two extra parameters.

--- hippy.py
+++ hippy.py
@@ -32,23 +32,29 @@ def stepsize(v, dv):
 
 class normalequations:
 
-    def __init__(self, A, x, s):
+    def __init__(self, A, x, s, z, w):
         '''Constructor.'''
         self.A = A
         self.x = x
+        self.z = z
         self.d = s / x
-        self.D = 1.0 / (self.d)
+        self.f = w / z
+        self.D = 1.0 / (self.d + self.f.todense())
         self.M = self.A * diag(self.D) * self.A.T
 
-    def solve(self, xib, xic, xim):
+    def solve(self, xib, xic, xim, xiu, xiz):
         '''Solve the normal equations system for the given right-hand side.'''
         t = xim / self.x
-        r = xic - t
+        v = xiz / self.z - xiu * self.f
+        r = xic - t + v.todense()
         rhs = self.A * (self.D * r) + xib
         dy = linsolve.spsolve(self.M, rhs)
         dx = self.D * (self.A.T * dy - r)
         ds = t - dx * self.d
-        return dx, dy, ds
+        xx = dx[xiu.idx]
+        dz = xiu - xx
+        dw = v + self.f * xx
+        return dx, dy, ds, dz, dw
 
 class hippy:
 
@@ -63,37 +69,45 @@ class hippy:
 
     def direction(self):
         '''Build the Newton system and compute the search direction.'''
-        NE = normalequations(self.A, self.x, self.s)
+        NE = normalequations(self.A, self.x, self.s, self.z, self.w)
 
-        (dx, dy, ds) = self.newton(NE, self.x, self.s)
-        (dx, dy, ds) = self.mehrotra(NE, dx, dy, ds)
-        self.alphap = 0.9995 * stepsize(self.x, dx)
-        self.alphad = 0.9995 * stepsize(self.s, ds)
+        dx, dy, ds, dz, dw = self.newton(NE, self.x, self.s, self.z, self.w)
+        dx, dy, ds, dz, dw = self.mehrotra(NE, dx, dy, ds, dz, dw)
+        self.alphap = 0.9995 * min(stepsize(self.x, dx),
+                                   stepsize(self.z.array(), dz.array()))
+        self.alphad = 0.9995 * min(stepsize(self.s, ds),
+                                   stepsize(self.w.array(), dw.array()))
 
-        self.makestep(dx, dy, ds)
+        self.makestep(dx, dy, ds, dz, dw)
         self.xi()
 
-    def newton(self, NE, x, s, mu = 0.0):
+    def newton(self, NE, x, s, z, w, mu = 0.0):
         '''Compute the affine-scaling direction.'''
         v = -x * s + mu
-        return NE.solve(self.xib, self.xic, v)
+        t = -z * w + mu
+        return NE.solve(self.xib, self.xic, v, self.xiu, t)
 
-    def sigmamu(self, dx, ds):
+    def sigmamu(self, dx, ds, dz, dw):
         '''Compute the target barrier parameter for Mehrotra's corrector.'''
-        alphap = stepsize(self.x, dx)
-        alphad = stepsize(self.s, ds)
+        alphap = min(stepsize(self.x, dx), stepsize(self.z.array(), dz.array()))
+        alphad = min(stepsize(self.s, ds), stepsize(self.w.array(), dw.array()))
         x = self.x + alphap * dx
         s = self.s + alphad * ds
-        g = self.average(x, s)
+        z = self.z + alphap * dz
+        w = self.w + alphad * dw
+        g = self.average(x, s, z, w)
         return g**3 / self.mu**2
 
-    def mehrotra(self, NE, dx, dy, ds):
+    def mehrotra(self, NE, dx, dy, ds, dz, dw):
         '''Compute Mehrotra's corrector.'''
-        v = -dx * ds + self.sigmamu(dx, ds)
+        m = self.sigmamu(dx, ds, dz, dw)
+        v = -dx * ds + m
+        t = -dz * dw + m
         zb = [0.0] * len(self.xib)
         zc = [0.0] * len(self.xic)
-        mx, my, ms = NE.solve(zb, zc, v)
-        return dx + mx, dy + my, ds + ms
+        zu = Sparsevector(self.n, [0.0] * len(t), t.idx)
+        mx, my, ms, mz, mw = NE.solve(zb, zc, v, zu, t)
+        return dx + mx, dy + my, ds + ms, dz + mz, dw + mw
 
     def read(self):
         '''Read the MPS file.'''
@@ -120,8 +134,8 @@ class hippy:
 
         # compute an initial point
         # D = diag(1.0 if no upper bound; 0.5 if upper bound)
-        # ADA^Tv = b - ADu   x = DA^Tv + Du
-        # ADA^Ty = ADc       s = Dc - DA^Ty
+        # ADA^Tv = b - Adu   x = DA^Tv + Du   z = u - x
+        # ADA^Ty = ADc       s = Dc - DA^Ty   w = -s
         d = [1.0] * self.n
         for i in self.u.idx:
             d[i] = 0.5
@@ -131,23 +145,36 @@ class hippy:
         x = F.T * v + d * u
         y = linsolve.spsolve(M, F * self.c)
         s = d * self.c - F.T * y
+        z = self.u - x[self.u.idx]
+        w = Sparsevector(self.n, -s[self.u.idx], self.u.idx)
 
-        # shift the point
-        # dp = max(-1.5 * min { x_i }, 0.1)
-        # dd = max(-1.5 * min { s_i }, 0.1
-        # xs = (x + dp)^T (s + dd) / x^Ts
-        # dp = dp + 0.5 * xs / sum(x + dp)
-        # dd = dd + 0.5 * xs / sum(s + dd)
-        # x = x + dp,  s = s + dd
+        # compute the shift
+        # dp = max{-1.5 * min {x_i}, -1.5 * min {z_i}, 0.1}
+        # dd = max{-1.5 * min {s_i}, -1.5 * min {w_i}, 0.1}
         dp = max(-1.5 * min(x), 0.1)
         dd = max(-1.5 * min(s), 0.1)
-        xs = (x + dp).T * (s + dd)
-        dp += 0.5 * xs / sum(x + dp)
-        dd += 0.5 * xs / sum(s + dd)
+        if len(self.u) > 0:
+            dp, dd = max(dp, -1.5 * min(z.val)), max(dd, -1.5 * min(w.val))
 
+        # update the shift
+        # this is a simplified version which doesn't take into account the
+        # contribution of z and w. note that the shift is not uniform for
+        # problems without upper bounds.
+        xs = (x + dp) * (s + dd)
+        if len(self.u) > 0:
+            xs = sum(xs)
+        dp += 0.5 * xs / (sum(x + dp) + sum(z + dp))
+        dd += 0.5 * xs / (sum(s + dd) + sum(w + dd))
+
+        # shift the point
         self.x = x + dp
         self.y = y
         self.s = s + dd
+        if len(self.u) == 0:
+            dp, dd = dp[z.idx], dd[w.idx]
+        self.z = z + dp
+        self.w = w + dd
+
         self.xi()
 
     def initpoint(self, point):
@@ -160,19 +187,20 @@ class hippy:
             print "The vectors given to initpoint() have the wrong dimensions."
             raise
 
-    def average(self, x, s):
+    def average(self, x, s, z, w):
         '''Compute the average complementarity gap.'''
-        gap = dot(x, s)
-        return gap / self.n
+        gap = dot(x, s) + dot(z.array(), w.array())
+        return gap / (self.n + len(z))
 
     def xi(self):
         '''Compute duality gap, complementarity gap and infeasibilities.'''
         self.pobj = dot(self.c, self.x)
-        self.dobj = dot(self.b, self.y)
+        self.dobj = dot(self.b, self.y) - dot(self.u.array(), self.w.array())
         self.gap = self.pobj - self.dobj
-        self.mu  = self.average(self.x, self.s)
+        self.mu  = self.average(self.x, self.s, self.z, self.w)
         self.xib = self.b - self.A * self.x
-        self.xic = self.c - self.A.T * self.y - self.s
+        self.xic = self.c - self.A.T * self.y - self.s + self.w.todense()
+        self.xiu = self.u - self.z - self.x[self.u.idx]
         self.erb = max(abs(self.xib))
         self.erc = max(abs(self.xic))
 
@@ -187,12 +215,14 @@ class hippy:
         print "%3d %10.3e %10.3e %10.3e %10.3e %10.3e %10.3e" % \
               (self.iter, alphap, alphad, erb, erc, self.mu, self.gap)
 
-    def makestep(self, dx, dy, ds):
+    def makestep(self, dx, dy, ds, dz, dw):
         '''Move along the given direction.'''
         (alphap, alphad) = (self.alphap, self.alphad)
         self.x += alphap * dx
         self.y += alphad * dy
         self.s += alphad * ds
+        self.z += alphap * dz
+        self.w += alphad * dw
 
     def info(self):
         '''Report statistics on the solution.'''
@@ -238,13 +268,15 @@ class hippy:
 
     def getsolution(self):
         '''Retrieve the solution vectors.'''
-        return (self.x, self.y, self.s)
+        return (self.x, self.y, self.s, self.z, self.w)
 
     def printiter(self):
         '''Print the primal-dual iterate.'''
         print "x:", self.x
         print "y:", self.y
         print "s:", self.s
+        print "z:", self.z
+        print "w:", self.w
 
 def usage():
     '''Report the usage message.'''
